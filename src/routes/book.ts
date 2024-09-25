85aaaca3b713519f735986eda0e74dd8740bfc16
sbayce <youssef2311@gmail.com>
2024-09-25 18:49:39 +0300

    rate limiter

--- src/routes/book.ts
+++ src/routes/book.ts
@@ -4,11 +4,20 @@ import searchBooks from '../controllers/book/search-books'
 import addBook from '../controllers/book/add-book'
 import deleteBook from '../controllers/book/delete-book'
 import updateBook from '../controllers/book/update-book'
+import { rateLimit } from "express-rate-limit"
+
+// rate limiter middleware
+const limiter = rateLimit({
+    windowMs: 60000, // 1 minute time window
+    limit: 10, // 10 requests
+    standardHeaders: true,
+    legacyHeaders: false
+})
 
 const router = Router()
 
-router.get('/all', getBooks)
-router.get('/search', searchBooks)
+router.get('/all', limiter, getBooks)
+router.get('/search', limiter, searchBooks)
 router.post('/add', addBook)
 router.delete('/delete/:bookId', deleteBook)
 router.patch('/update/:bookId', updateBook)
